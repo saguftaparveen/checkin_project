04948d20446a02c553606d868d840282d98acba6
Rekha-sys0 <sagparveen786@gmail.com>
2021-12-28 10:09:49 +0530

    location added

--- lib/splash.dart
+++ lib/splash.dart
@@ -16,7 +16,7 @@ class _SplashState extends State<Splash> {
   }
 
   _navigatetohome() async {
-    await Future.delayed(Duration(seconds: 20), () {});
+    await Future.delayed(Duration(seconds: 3), () {});
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => MyHomePage()));
   }
--- lib/user_home.dart
+++ lib/user_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:checkin_project/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 class UserHome extends StatefulWidget {
   UserHome({Key? key}) : super(key: key);
@@ -7,9 +9,37 @@ class UserHome extends StatefulWidget {
   _UserHomeState createState() => _UserHomeState();
 }
 
+var address = "hi";
+
 class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  shagufta();
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xfff99820),
+              ),
+              child: Text("Checkin")),
+          Text(address)
+        ],
+      ),
+    );
   }
+}
+
+shagufta() async {
+  Position position = await getGeoLocationPosition();
+  String location = 'Lat: ${position.latitude} , Long: ${position.longitude}';
+
+  address = await GetAddressFromLatLong(position);
+
+  print(address);
 }
